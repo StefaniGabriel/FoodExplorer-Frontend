bd76945fe3bd95042cf89e6a375f51361522c57a
Stefani Gabriel <111387502+StefaniGabriel@users.noreply.github.com>
2023-05-10 10:15:42 -0300

    commit

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+export default {
+    COLORS: {
+        BACKGROUND_BLUE900: "#1E1E1E",
+        BACKGROUND_BLUE800: "#001119",
+
+        BLUE_700: "#065E7C",
+
+        BACKGROUND_RED900: "#750310",
+
+        WHITE_900: "#FFFFFF",
+
+        GRAY_900: "#7C7C8A"
+    }
+}
